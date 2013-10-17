9d2cbf77a42334e83acc268e127c432f9124512c
sebaschan <tebusco@Sebastian-R.local>
2013-10-17 11:28:59 -0500

    ahi te va

--- www/js/responsivemobilemenu.js
+++ www/js/responsivemobilemenu.js
@@ -54,10 +54,10 @@ function getMobileMenu() {
 	$('.rmm').each(function() {	
 				var menutitle = $(this).attr("data-menu-title");
 				if ( menutitle == "" ) {
-					menutitle = "Menu";
+					menutitle = "Buscar por";
 				}
 				else if ( menutitle == undefined ) {
-					menutitle = "Menu";
+					menutitle = "Buscar por";
 				}
 				var $menulist = $(this).children('.rmm-main-list').html();
 				var $menucontrols ="<div class='rmm-toggled-controls'><div class='rmm-toggled-title'>" + menutitle + "</div><div class='rmm-button'><span>&nbsp;</span><span>&nbsp;</span><span>&nbsp;</span></div></div>";
